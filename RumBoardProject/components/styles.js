52dc2481c6ed014632c4d53148c59cc6f5b101f0
jonathan <jonathan.figueroa16@upr.edu>
2022-03-18 22:51:19 -0400

    Implement welcome pages with login features

---
+++ RumBoardProject/components/styles.js
@@ -0,0 +1,107 @@
+import styled from 'styled-components';
+import {View, Text, Image, TextInput, TouchableOpacity} from 'react-native';
+import Constants from 'expo-constants';
+
+const StatusBarHeight = Constants.statusBarHeight;
+
+//colors 
+export const Colors ={
+    primary: '#ffffff',
+    secondary: '#E5E7EB',
+    tertiary: '#1F2937',
+    darkLight: '#9CA3AF',
+    brand: '#6D28D9',
+    green: '#10B981',
+    red: '#EF4444',
+
+};
+
+const { primary, secondary, tertiary, darkLight, brand, green, red } = Colors;
+
+export const StyledContainer = styled.View`
+    flex: 1;
+    padding: 25px;
+    padding-top: ${StatusBarHeight + 20}px;
+    background-color: ${tertiary};
+`
+
+export const InnerContainer = styled.View`
+    flex: 1;
+    width: 100%;
+    align-items: center;
+
+`;
+
+export const PageLogo = styled.Image`
+    width; 250px;
+    height: 300px;
+
+`;
+export const PageTitle = styled.Text`
+    font-size: 30px;
+    text-align: center;
+    font-weight: bold;
+    color: ${green};
+    padding: 10px;
+
+`;
+
+export const SubTitle = styled.Text`
+    font-size = 18px;
+    margin-bottom: 20px;
+    letter-spacing: 1px;
+    font-weight: bold;
+    color: ${primary};
+
+`;
+
+export const StyledFormArea = styled.View`
+    width: 90%;
+`;
+
+export const StyledTextInput = styled.TextInput`
+    background-color: ${secondary};
+    padding: 15px;
+    padding-left: 55px;
+    padding-right: 55px;
+    border-radius: 5px;
+    font-size: 16px;
+    height: 60px;
+    margin-vertical: 3px;
+    margin-bottom: 10px;
+    color: ${tertiary};
+`;
+
+export const StyledInputLabel = styled.Text`
+    color: ${green};
+    font-size: 13px;
+    text-align: left;
+`;
+
+export const LeftIcon = styled.View`
+    left: 15px;
+    top: 38px;
+    position: absolute;
+    z-index: 1;
+`;
+
+export const RightIcon = styled.TouchableOpacity`
+    right: 15px;
+    top: 38px;
+    position: absolute;
+    z-index: 1;
+`;
+
+export const StyledButton = styled.TouchableOpacity`
+    padding: 15px;
+    background-color: ${green};
+    justify-content: center;
+    border-radius: 5px;
+    margin-vertical: 5px;
+    height: 60px;
+`;
+
+export const ButtontText = styled.Text`
+    color: ${tertiary};
+    font-size: 16px;
+`;
