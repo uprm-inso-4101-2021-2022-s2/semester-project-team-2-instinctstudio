a0bb2c0e2e38e1d9e88e92e5489633914eeb0eb5
jonathan <jonathan.figueroa16@upr.edu>
2022-03-18 22:58:47 -0400

    Implement the welcome screen for the with logins features

--- RumBoardProject/App.js
+++ RumBoardProject/App.js
@@ -1,20 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import React from 'react';
+
+
+
+// screens
+import Login from './screens/Login';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+  return (<Login />
+    
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ RumBoardProject/screens/Login.js
@@ -0,0 +1,106 @@
+import React, {useState} from 'react';
+import { StatusBar } from 'expo-status-bar';
+import {View} from 'react-native';
+
+// formik 
+import{Formik} from 'formik';
+
+
+//icons
+import {Octicons, Ionicons} from '@expo/vector-icons';
+
+import {
+    StyledContainer,
+    InnerContainer,
+    PageLogo,
+    PageTitle,
+    SubTitle,
+    StyledFormArea,
+    LeftIcon,
+    StyledInputLabel,
+    StyledTextInput,
+    RightIcon,
+    StyledButton,
+    ButtontText,
+    Colors
+} 
+from './../components/styles';
+
+const {primary, darkLight, secondary,tertiary , green } = Colors;
+
+const Login = () => {
+    const [hidePassword, setHidePassword] = useState(true);
+    return (
+        <StyledContainer>
+            <StatusBar style = "dark"/>
+            <InnerContainer>
+                {/* Code for the welcome screen implement logo  */}
+                <PageLogo resizeMode = "cover" source = {require('./../assets/logo(1).png')} />
+                <PageTitle>RUM BOARD</PageTitle>
+                <SubTitle>Account Login</SubTitle>
+
+
+                <Formik
+                    initialValues = {{email: '', password: ''}}
+                    onSubmit = {(values) => {
+                        console.log(values);
+                    }}
+
+                >
+                    {({handleChange, handleBlur, handleSubmit, values }) => (
+                    <StyledFormArea> 
+                        {/* Code for the login feature */}
+                        <MyTextInput
+                         label = "Email Address"
+                         icon = "mail"
+                         placeholder = "jonathan.figueroa16@upr.edu"
+                         placeholderTextColor = {tertiary}
+                         onChangeText = {handleChange('email')}
+                         onBlur = {handleBlur('email')}
+                         value = {values.email}
+                         KeyboardType = "email-address" 
+                    />
+
+                      <MyTextInput
+                         label = "Password"
+                         icon = "lock"
+                         placeholder = "* * * * * * * * *"
+                         placeholderTextColor = {tertiary}
+                         onChangeText = {handleChange('password')}
+                         onBlur = {handleBlur('password')}
+                         value = {values.password}
+                         secureTextEntry={hidePassword}
+                         isPassword={true}
+                         hidePassword = {hidePassword}
+                         setHidePassword = {setHidePassword}
+                    />
+                    <StyledButton >
+                        <ButtontText>
+                            Login
+                        </ButtontText>
+                    </StyledButton>
+                         
+                    </StyledFormArea>)}
+                </Formik>
+            </InnerContainer>
+        </StyledContainer>
+    );
+};
+
+const MyTextInput = ({label, icon, isPassword, hidePassword, setHidePassword, ...props }) => {
+    return (<View>
+            <LeftIcon>
+            <Octicons name = {icon} size = {30} color = {tertiary} />
+            </LeftIcon>
+            <StyledInputLabel>{label}</StyledInputLabel>
+            <StyledTextInput {...props} />
+            {isPassword && (
+            // Code for the password, see and not see the password 
+            <RightIcon onPress ={() => setHidePassword(!hidePassword)}>
+                <Ionicons name = {hidePassword ? 'md-eye-off' : 'md-eye'}    size = {30} color = {green} />
+            </RightIcon>)}
+    </View>)
+
+};
+
+export default Login;
